6057cdde2a7e6cac0e5436ce7f614778b2eb325d
Chris Stuckey <chrisstuckey@Lilianas-MacBook-Pro.local>
2017-05-15 16:37:24 -0400

    have a bunch files and code not running

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger.');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Bacon Burger.');
+INSERT INTO burgers (burger_name) VALUES ('Five-Cheese Cheeseburger.');
